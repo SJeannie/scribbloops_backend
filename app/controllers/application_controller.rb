238b93b5565cb56ebb8888fc607d732642559cf1
pabloargueta <pabloargueta27@gmail.com>
2018-10-01 17:52:35 -0500

    Working on authentication encoding tokens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+
+  def encode_token(payload)
+    JWT.encode(payload, 'app_wide_secret_for_every_user')
+  end
+
+
+  def decoded_token(token)
+    JWT.decode(token, 'app_wide_secret_for_every_user')[0]
+  end
+
 end
